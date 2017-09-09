f0c4ad31fa1c5665080dc707a166518bf4cc9890
myanguyen98 <waddio98@gmail.com>
2017-09-09 09:15:32 -0600

    first commit

--- BasicCard.js
+++ BasicCard.js
@@ -1,13 +1,28 @@
+var inquirer = require("inquirer");
 function BasicCard(question,answer){
 		this.question = question;
 		this.answer = answer;
 		this.printCard = function(){
-			console.log("Answer the question: "+this.question+
-				"\nYour answer: "+//take user input here
-				"\nThe correct answer was: "+this.answer)
+			inquirer.prompt([
+			{
+				name:"question",
+				message:this.question
+			}
+			]).then(function(inquirerResponse){
+				if (inquirerResponse.question === this.answer) {
+					console.log("You answered the question correctly!")
+				} else {
+					console.log("Sorry, the correct answer was" + this.answer)
+				}
+			})
 		}
+		this.printCard();
 
 };
+
+var quest1 = new BasicCard("Who was the 16th president of the United States?","Abraham Lincoln");
+var quest2 = new BasicCard("Who was the 3rd president of the United States?","Thomas Jefferson");
+
 
 //Example of adding new flashcard to existing constructor
 /*var firstPresident = new BasicCard(
